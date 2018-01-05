0aa6795dd388e207401dea6670ee6bd064f405ed
Víctor <yomellamovictor@gmail.com>
2018-01-05 11:03:55 +0100

    Update 3.custom.sh

--- 3.custom.sh
+++ 3.custom.sh
@@ -67,6 +67,6 @@ source ~/.zshrc
 # Hides last login
 touch ~/.hushlogin
 
-defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
-defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
+defaults write -g InitialKeyRepeat -int 15 # normal minimum is 15 (225 ms)
+defaults write -g KeyRepeat -int 2 # normal minimum is 2 (30 ms)
 
